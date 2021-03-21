26643b15de4a8e97d6f2d43bd6659f15d872adaa
mahaboobtech <52873899+mahaboobtech@users.noreply.github.com>
2021-03-21 16:06:33 +0530

    Update getpipdep.py

--- getpipdep.py
+++ getpipdep.py
@@ -1,13 +1,13 @@
 import re
 
-f = open("/home/$username/anyfolder/file.txt",'r+')
+f = open("file.txt",'r+')
 pata = f.read()
 comp = []
 comp.append(re.findall(r"^.[^-]{22}",pata,flags=re.MULTILINE))
 f.close()
 print(len(comp[0]))
 print(comp[0])
-g = open("/home/$username/Desktop/pipdep.txt","w+")
+g = open("pipdep.txt","w+")
 for l in range (len(comp[0])):
     g.writelines(str(comp[0][l]))
     g.write("\n")
